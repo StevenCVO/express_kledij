531a64f07cb8aad4c30bcdacfcb0f77bd9d9c9d6
unknown <scripter@BK102PC25.sjca.lan>
2019-02-12 22:08:56 +0100

    Eerste commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const { producten } = require("./producten");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Dank u voor de aanvraag!");
+});
+
+app.get("/producten", (req, res) => {
+    res.send(producten);
+});
+
+app.get("/producten/:id", (req, res) => {
+    const product = producten.find(product => {
+        // Om het id uit het pad vast te krijgen
+        // gebruiken we het params object uit het
+        // request object (req)
+        return product.id === +req.params.id;
+    });
+
+    if (!product) {
+        res.status(404).send("Product niet gevonden");
+    }
+    
+    res.send(product);
+})
+
+app.get("*", (req, res) => {
+    res.status(404).send("Ongeldig eindpunt");    
+})
+
+app.listen(3333, () => {
+    console.log(`Server runt op poort 3333`);
+});
